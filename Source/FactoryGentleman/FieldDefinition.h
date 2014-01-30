27d57148f8c1128e07947f9a7c85c02009dc87a9
Slavko Krucaj <slavko@soundcloud.com>
2014-01-30 17:05:31 +0100

    Added FieldDefinition

---
+++ Source/FactoryGentleman/FieldDefinition.h
@@ -0,0 +1,7 @@
+@interface FieldDefinition : NSObject
+@property (nonatomic, readonly) NSString *name;
+@property (nonatomic, readonly) id (^definition)();
+
+- (instancetype)init __attribute__((unavailable("init not available ")));
++ (instancetype)withFieldName:(NSString *)name definition:(id (^)())definition;
+@end
